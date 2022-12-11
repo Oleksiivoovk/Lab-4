c4145a0cb2e128df08334812da8359c47e21f0e7
Oleksiivoovk <errrfe1@gmail.com>
2022-12-11 19:02:39 +0200

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ukr");
+    int n, i;
+    int arr[100];
+    int b, c, s;
+    cout << "Кiлькiсть елементiв масиву ";
+    cin >> n;
+        cout << "Номери масиву ";
+    cin >> b >> c;
+    cout << "елементи масиву ";
+    for (i = 1; i <= n; i++) {
+        
+        cin >> arr[i];
+    }
+    s = (arr[b] + arr[c]) / 2;
+   
+    cout << "Вiдповiдь " << endl;
+    
+    for (i = 1; i <= n; i++) {
+        if (arr[i] % s == 0) {
+            cout  << arr[i] << endl;
+        }
+    }
+
+    return 0;
+}
